7b0323cde8c183ef3844528f4d1c9711482d8eed
frankdavidcorona <frankdavid.corona@gmail.com>
2018-01-17 11:02:39 -0500

    Fixe base.

--- source/src/components/index/Index.js
+++ source/src/components/index/Index.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                Hola EMR
+                Base React Project
             </div>
         );
     }
